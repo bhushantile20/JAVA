02a0fee2e864a207a8a9d519bb18a11d2cc98fba
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-07 22:55:43 +0530

    Add files via upload

---
+++ LetterPositionCount.java
@@ -0,0 +1,17 @@
+public class LetterPositionCount
+{
+    public static void main(String args[])
+    {
+         String s="BHUSHAN BHAU";
+         char[] a=s.toCharArray();
+         int i=1;
+         {
+             for(char output:a)
+             {
+                 System.out.println(output+""+i+"");
+                 i++;
+                 
+             }
+         }
+    }
+}
