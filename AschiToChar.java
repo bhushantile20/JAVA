d7c2fdb3f41624880a683ac37f39f080bc68b2f2
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-07 22:55:24 +0530

    Add files via upload

---
+++ AschiToChar.java
@@ -0,0 +1,13 @@
+public class AschiToChar
+{
+    public static void main(String args[])
+    {
+        char c;
+        for(int i=65;i<=90;i++)
+        {
+            c=(char)i;
+            System.out.println(i+"="+c);
+            
+        }
+    }
+}
