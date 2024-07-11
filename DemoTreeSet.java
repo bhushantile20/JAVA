26801b592296324f885ae8fb1ea910ad7139098f
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-11 22:49:09 +0530

    Add files via upload

---
+++ DemoTreeSet.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+public class DemoTreeSet
+{
+    public static void main(String args[])
+    { 
+        TreeSet<String> tree_set=new TreeSet<String>();
+        tree_set.add("Red");
+        tree_set.add("Green");
+        tree_set.add("Orange");
+
+
+        System.out.println("Trees ser:");
+        System.out.println(tree_set);
+        
+    }
+}
