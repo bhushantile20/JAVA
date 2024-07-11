bb20fcaee897b4b8476bb5aaaaead8fb9dff546e
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-11 23:04:38 +0530

    LinkedList display the elements

---
+++ Demo3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Demo3
+{
+    public static void main(String args[])
+    {
+         LinkedList<String>a1=new LinkedList<String>();
+         a1.add("java programming");
+         a1.add("java Programming:");
+         a1.add("operating system:");
+         a1.add("Softare couese:");
+
+         Iterator<String>it=a1.iterator();
+         while(itr.hashNext())
+         {
+            System.out.println(itr.next());
+
+         }
+    }
+}
