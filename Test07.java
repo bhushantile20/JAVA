8388991ee9e48910806afa197c31b0d18e246485
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-19 23:45:18 +0530

    Add files via upload

---
+++ Test07.java
@@ -0,0 +1,31 @@
+class Test07
+{
+    
+    public static void main(String[] args) {
+
+
+        System.out.println("bhushan");
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("A");
+
+        System.out.println("ab");
+        System.out.println("1");
+        System.out.println("10");
+        System.out.println("abc@123");
+        
+
+
+        String name="HK";
+
+        String course="core java";
+        String address="Bhushan f23 445 2,423432 64,hyd , 7654";
+      
+        System.out.println("nmae:\t"+name);
+        System.out.println("course:\t"+course);
+        System.out.println("address:\t"+address);
+
+
+    }
+}
