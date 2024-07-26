51e2f7a2f270d171aa5dd391504fd46453b43c36
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-26 21:55:59 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Sum
+{
+	public static void main(String[] args) 
+	{
+		int x,y,sum;
+
+         Scanner sc=new Scanner(System.in);
+System.out.println("Enter a first no:");
+
+         x=sc.nextInt();
+System.out.println("Enter a secons no:");
+         y=sc.nextInt();
+         
+         sum=x+y;
+
+      System.out.println("sum is :"+sum);
+     
+	}
+}
