1585846a8a5ef51c9005de4cc32a24a58e6dc6d8
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-08-05 21:36:38 +0530

    Add files via upload

---
+++ all.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class all
+{
+	public static void main(String[] args) 
+	{
+	       Scanner sc=new Scanner(System.in);
+           
+           System.out.println("Enter a 2 numbers:");
+           int a=sc.nextInt();
+           int b=sc.nextInt();
+               
+               int c=a+b;
+               int d=a-b;
+               int e=a*b;
+               int f=a/b;
+              System.out.println("Add sub mul and divsion is:"+c +d +e +f);	
+    	}
+    }
+    
