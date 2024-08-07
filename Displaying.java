9fa0978372fac7242660b473f6475116077ec974
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-08-07 23:36:34 +0530

    Add files via upload

---
+++ Displaying.java
@@ -0,0 +1,20 @@
+class Displaying 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Screen Display");
+		 for(int i=1;i<=9; i++)
+
+{
+ for (int j=1;j<=i ; j++ )
+ {
+   System.out.println(" ");
+System.out.println(i);
+
+ }
+System.out.println("\n");
+
+	}
+System.out.println("Screen Display Done:");
+}
+}
