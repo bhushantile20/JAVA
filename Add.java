273a304a88959ca4579d136909334a496e48b05b
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-08-10 23:55:46 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Add
+{
+	public static void main(String[] args) 
+	{
+	     Scanner sc=new Scanner(System.in);
+System.out.println("Enter 1 number");
+           int a=sc.nextInt();
+System.out.println("Enter 2 number:");
+           int b=sc.nextInt();
+           int c=a+b;
+
+         System.out.println("Addition is :"+c);
+
+	}
+}
