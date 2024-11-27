b9644663f18d6fa09a3aa0012dad75775f0f042b
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-11-27 23:02:59 +0530

    Add files via upload

---
+++ BreakExample.java
@@ -0,0 +1,15 @@
+public class BreakExample {
+    public static void main(String args[])
+
+    {
+        for(int i=0;i<=30;i++)
+
+        {
+             System.out.println(i);
+             if(i==10)
+             {
+                break;
+             }
+        }
+}
+}
---
+++ Caculation.java
@@ -0,0 +1,11 @@
+public class Caculation {
+    public static void main(String argsp[])
+    {
+        int i=0;
+        System.out.println("Printing the list of first 10 even numbers\n");
+        while(1<=10){
+             System.out.println(i);
+             i=i+2;
+        }
+    }
+
