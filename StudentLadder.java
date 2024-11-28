182f8bb7eb2d6c709c2b1df1e979d58de9bc8b9b
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-11-28 18:28:49 +0530

    Add files via upload

---
+++ StudentLadder.java
@@ -0,0 +1,22 @@
+public class StudentLadder {
+    public static void main(String args[])
+    {
+         String city="Nashik";
+
+         if(city=="Merrut")
+         {
+             System.out.println("City is merrut");
+         }
+         else if(city=="Noida")
+         {
+             System.out.println("City is noida");
+         }
+         else if(city=="Agra")
+         {
+             System.out.println("CIrt is Agra");
+         }
+         else{
+             System.out.println(city);
+         }
+    }
+}
