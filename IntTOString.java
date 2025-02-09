4c39e5b93f9cca1a077cefb1fdcb2fa5438d01fb
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2025-02-10 00:19:37 +0530

    Type Conversion
    int to string and string to float

---
+++ IntTOString.java
@@ -0,0 +1,8 @@
+public class IntTOString {
+    public static void main(String args[]) {
+        int i = 200;
+        String s = String.valueOf(i);
+        System.out.println(i + 100);
+        System.out.println(s + 100);
+    }
+}
---
+++ StringToFloat.java
@@ -0,0 +1,8 @@
+public class StringToFloat {
+    public static void main(String[] args)
+    {
+         String s="23.6";
+         float f=Float.parseFloat("23.6");
+         System.out.println(f);
+    }
+}
